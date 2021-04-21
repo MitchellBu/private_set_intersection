2e9caa5340a1d1428a28e0404736d92e2e8370c9
Mitchell Butovsky <butovsky.mitchell@gmail.com>
2021-04-21 22:11:27 +0300

    Changed encryption scheme to 'Paillier', inclding the implementation of the homomorphic properties.

--- utils.py
+++ utils.py
@@ -2,10 +2,43 @@ import random
 import numpy as np
 import os
 
+
+class MathUtils(object):
+
+    def _extended_gcd(self, num1, num2):
+        ''' Euclidean algorithm implementation '''
+        gcd, scalar_1, scalar_2 = 0, 0, 0
+        a = np.array([1, 0], dtype=int)
+        b = np.array([0, 1], dtype=int)
+        while (num2 and num1):
+            if num2 > num1:
+                x = num2 // num1
+                y = num2 % num1
+                num2 = y
+                b -= x*a
+            else:
+                x = num1 // num2
+                y = num1 % num2
+                num1 = y
+                a -= x*b
+        if num1 == 0:
+            gcd,scalar_1, scalar_2 = num2, b[0], b[1]
+        else:
+            gcd, scalar_1, scalar_2 = num1, a[0], a[1]
+        return gcd, scalar_1, scalar_2
+
+    def gcd(self, num1, num2):
+        ''' Get the greatest common divisor of num1 and num2'''
+        return self._extended_gcd(num1, num2)[0]
+
+    def inverse(self, num, modulo):
+        ''' Find the modular inverse of num '''
+        return ((self._extended_gcd(num, modulo))[1] % modulo)
+
 class PrimeGenerator(object):
 
     def __init__(self, num_of_bits, confidence):
-        ''' generate num_of_bits bits number which is a prime of the form 2*p + 1 with high probability \
+        ''' generate num_of_bits bits number which is a prime with high probability \
         failure probabilty upper bound exponentialy decreases as confidence increases '''
         self.num_of_bits = num_of_bits
         self.confidence = confidence
@@ -13,14 +46,12 @@ class PrimeGenerator(object):
     def get_prime(self):
         ''' generate the prime '''
         is_prime = False
-        is_special_prime = False # prime p such that 2*p + 1 is also a prime
         prime_candidate = 0
-        while (not is_prime) or (not is_special_prime):
-            rand_bits = random.getrandbits(self.num_of_bits - 2)
-            prime_candidate = 2 ** (self.num_of_bits - 2) + rand_bits #Assure that the MSB bit is 1. 
+        while not is_prime:
+            rand_bits = random.getrandbits(self.num_of_bits - 1)
+            prime_candidate = 2 ** (self.num_of_bits - 1) + rand_bits #Assure that the MSB bit is 1. 
             is_prime = self._primality_check(prime_candidate)
-            is_special_prime = self._primality_check(2 * prime_candidate + 1)
-        return (2 * prime_candidate + 1)
+        return prime_candidate
             
     def _primality_check(self, number):
         ''' Miller-Rabin primality test implementation '''
@@ -44,59 +75,90 @@ class PrimeGenerator(object):
                 return False
         return True
 
+class Ciphertext(object):
+
+    def __init__(self, ciphertext, modulo):
+        self.ciphertext = ciphertext
+        self.modulo = modulo
+
+    def __add__(self, other):
+        ''' Homomorphic encryption for the addition of the plaintexts '''
+        if other.modulo != self.modulo:
+            raise("Error: ciphertext modulo mismatch.")
+        return Ciphertext((self.ciphertext * other.ciphertext) % (self.modulo ** 2), self.modulo)
+
+    def __mul__(self, num):
+        ''' Homomorphic encryption for the multiplication by scalar of the plaintext '''
+        effective_cipher = self.ciphertext
+        if num < 0:
+            effective_cipher = MathUtils().inverse(self.ciphertext, self.modulo ** 2)
+            num = -num
+        return Ciphertext(pow(effective_cipher, num, self.modulo ** 2), self.modulo)
+
+    def __str__(self):
+        return str(self.ciphertext)
+
 class EncryptionScheme(object):
+
+    #TODO: Research and implement huge ints handling
 
     def __init__(self):
         self.p = None
         self.q = None
-        self.g = None
-        self.public_key = None
-        self.secret_key = None
-        self.lookup_dict = None
+        self.public_key = None # N = pq
+        self.secret_key = None # phi(N) = (p-1)(q-1)
 
-    def generate(self, security_param=256, prime_confidence=40, max_message=128): #ASCII table contains 128 chars
+    def generate(self, security_param=256, prime_confidence=10):
         self.p = PrimeGenerator(security_param, prime_confidence).get_prime()
-        self.q = self.p - 1 # |G| = q
-        self.g = 1
-        while pow(self.g, 2, self.p) == 1: #Sufficient condition since p is of the form p = 2q + 1
-            self.g = random.randint(2, self.q - 1)
-        self.lookup_dict = {}
-        group_element = 1
-        for exp in range(max_message):
-            self.lookup_dict[group_element] = exp
-            group_element = (group_element * self.g) % self.p
-
-    def gen_keys(self):
-        x = random.randint(1, self.q - 1)
-        self.public_key = pow(self.g, x, self.p)
-        self.secret_key = x
+        self.q = PrimeGenerator(security_param, prime_confidence).get_prime()
+        self.public_key = self.p * self.q
+        self.secret_key = self.public_key - self.p - self.q + 1 # (p-1)(q-1) = pq - p - q + 1
 
     def to_file(self, file_path, include_secret_key=False):
-        ''' Save the scheme to a compressed npz file '''
-        values = np.array([self.p, self.q, self.g, self.public_key])
+        ''' Save the scheme to a .npy file '''
+        values = np.array([self.public_key])
         if include_secret_key:
-            values = np.array([self.p, self.q, self.g, self.public_key, self.secret_key])
-        lookup_arr = np.array(list(self.lookup_dict.items()))
-        np.savez(file_path, values=values, lookup_arr=lookup_arr)
+            values = np.array([self.p, self.q, self.public_key, self.secret_key])
+        np.save(file_path, values)
 
     def from_file(self, file_path):
-        ''' Load the scheme from a compressed npz file '''
+        ''' Load the scheme from a .npy file '''
         with np.load(file_path) as scheme_data:
-            values = scheme_data['values']
             try: # assume secret key included
-                self.p, self.q, self.g, self.public_key, self.secret_key = tuple(values)
+                self.p, self.q, self.public_key, self.secret_key = tuple(scheme_data)
+                self.p = int(self.p)
+                self.q = int(self.q)
                 self.secret_key = int(self.secret_key)
             except:
-                self.p, self.q, self.g, self.public_key = tuple(values)
-            self.p = int(self.p)
-            self.q = int(self.q)
-            self.g = int(self.g)
+                self.public_key = scheme_data[0]
             self.public_key = int(self.public_key)
-            lookup_arr = scheme_data['lookup_arr']
-            self.lookup_dict = {int(key) : int(value) for [key, value] in lookup_arr}
 
-    #TODO: Add support for very large ints
-    #TODO: performance checks
+    def encrypt_single_message(self, plaintext):
+        ''' Encrypts single numeric plaintext '''
+        if not (type(plaintext) is int):
+            raise "Error: plaintext has to be of int type"
+        r = 0 
+        N = self.public_key
+        N_squared = N ** 2
+        while MathUtils().gcd(r, self.public_key) != 1:
+            r = random.randint(1, N - 1) # Assure that r in Zn*
+        cipher_1 = pow(1 + N, plaintext, N_squared) #(1+N)^m mod N^2
+        cipher_2 = pow(r, N, N_squared) #r^N mod N^2
+        final_cipher = (cipher_1 * cipher_2) % N_squared
+        return  Ciphertext(final_cipher, N)
+
+    def decrypt_single_ciphertext(self, ciphertext):
+        ''' Decrypts single ciphertext of Ciphertext type'''
+        if not (type(ciphertext) is Ciphertext):
+            raise "Error: ciphertext has to be of Ciphertext type"
+        N = self.public_key
+        N_squared = N ** 2
+        if self.secret_key is None:
+            raise('Error: secret key was not found.')
+        cipher = ciphertext.ciphertext
+        c_hat = pow(cipher, self.secret_key, N_squared) # c^phi(N) mod N^2
+        m_hat = int((c_hat - 1) / N)
+        return (m_hat * MathUtils().inverse(self.secret_key, N_squared)) % N    
         
     def encode_message(self, message):
         ''' Outputs ASCII codes of message chars'''
@@ -106,33 +168,30 @@ class EncryptionScheme(object):
         ''' Outputs decoded message from ASCII array'''
         return ''.join(chr(code) for code in encoded_message)
 
-    def _encrypt_single_code(self, code):
-        ''' Encrypts single ASCII code plaintext '''
-        y = random.randint(1, self.q - 1) # y <- {1,...,q-1}
-        encryptor = pow(self.g, y, self.p) # g^y mod p
-        powered_message = pow(self.g, int(code), self.p) # g^m mod p
-        powered_key = pow(self.public_key, y, self.p) # h^y mod p
-        ciphertext = [encryptor, (powered_message * powered_key) % self.p] # (g^y mod p, (g^m * h^y) mod p)
-        return ciphertext
-
     def encrypt_encoded_message(self, encoded_message_vector):
         ''' Outputs the encryption of a whole encoded message '''
-        ciphertext_vector = np.array([self._encrypt_single_code(code) for code in encoded_message_vector])            
+        ciphertext_vector = np.array([self.encrypt_single_message(message) for message in encoded_message_vector])            
         return ciphertext_vector
-
-    def _decrypt_single_ciphertext(self, ciphertext):
-        ''' Decrypts single ciphertext pair (.,.) to its corresponding ASCII code'''
-        if self.secret_key is None:
-            raise('Error: secret key was not found.')
-        encryptor = int(ciphertext[0]) # g^y mod p
-        encrypted_message = ciphertext[1] # (g^m * h^y) mod p
-        powered_message = (encrypted_message * pow(encryptor, self.q - self.secret_key, self.p)) % self.p # g^m = ((g^m * h^y) mod p / g^y mod p) mod p
-        return self.lookup_dict[powered_message] # g^m -> m
 
     def decrypt_to_encoded_message(self, ciphertext_vector):
         ''' Decrypts a whole ciphertext to the encoded plaintext'''
-        encoded_plaintext_vector = np.array([self._decrypt_single_ciphertext(ciphertext) for ciphertext in ciphertext_vector])
+        encoded_plaintext_vector = np.array([self.decrypt_single_ciphertext(ciphertext) for ciphertext in ciphertext_vector])
         return encoded_plaintext_vector
 
         
-      
+scheme = EncryptionScheme()
+scheme.generate(security_param=10)
+print("Scheme info: p: " + str(scheme.p) + ", q: " + str(scheme.q) + ", N: " + str(scheme.public_key) + ", phi: " + str(scheme.secret_key))
+message_1 = 100
+cipher_1 = scheme.encrypt_single_message(message_1)
+message_2 = 200
+cipher_2 = scheme.encrypt_single_message(message_2)
+cipher = cipher_1 + cipher_2 # Demonstration of additive homomorphic property! 
+restored_plain = scheme.decrypt_single_ciphertext(cipher)
+print("Original message: " + str(message_1 + message_2) + ", ciphertext: " + str(cipher) + ", restored plaintext: " + str(restored_plain))
+message_3 = 111
+cipher_3 = scheme.encrypt_single_message(message_3)
+scalar = 3
+cipher_3_mul = cipher_3 * scalar # Demonstration of scalar multiplication property!
+res_3 = scheme.decrypt_single_ciphertext(cipher_3_mul)
+print("Original message: " + str(message_3 * scalar) + ", ciphertext: " + str(cipher_3_mul) + ", restored plaintext: " + str(res_3))
